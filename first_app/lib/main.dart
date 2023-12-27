5e05705d9725494420c6454d4ce4b26f6c4a498e
karthik_s_m <karthiksrk40@gmail.com>
2023-12-27 23:38:59 +0530

    Used classes and made app gradient

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,13 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.greenAccent,
-        body: Center(
-          child: Text("Hey its me Karthik!"),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 230, 181, 126),
+          Color.fromARGB(255, 204, 110, 21),
+          Color.fromARGB(255, 148, 71, 0),
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          "Hey its me Karthik!",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28.0,
+          ),
+        ),
+      ),
+    );
+  }
 }
